0cbd14bbf613b46f729ec8f868f34c5f95d0706b
Alok Kumar Nayak <alokkumar.nayak2009@gmail.com>
2025-06-07 08:59:22 +0530

    07-06-2025

---
+++ 01.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("This is a Python script.") 
+print("Hello World")
+
+
+print("Hello World")
+print("This is a Python script.") 
+
